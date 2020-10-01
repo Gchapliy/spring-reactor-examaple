f21bfc45ecee73203c2030e7807879d6c2c5e1aa
Hryhorii <grishachapliy1@gmail.com>
2020-10-01 16:26:37 +0300

    Initial commit

---
+++ src/main/java/com/example/spring_reactor_examaple/SpringReactorExamapleApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_reactor_examaple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactorExamapleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReactorExamapleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spring_reactor_examaple/SpringReactorExamapleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_reactor_examaple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactorExamapleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
