c48b03915b92ef26da2f18ec1735c21bb075b884
Hryhorii <grishachapliy1@gmail.com>
2020-10-02 12:28:57 +0300

    flux and mono errors

---
+++ src/test/java/com/example/spring_reactor_examaple/flux_and_mono/CustomException.java
@@ -0,0 +1,19 @@
+package com.example.spring_reactor_examaple.flux_and_mono;
+
+public class CustomException extends Throwable {
+
+    private String message;
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public CustomException(Throwable e) {
+        this.message = e.getMessage();
+    }
+}
