48922e6f312df414819498df68587644e83ed29a
Hryhorii <grishachapliy1@gmail.com>
2020-10-06 15:07:06 +0300

    reactive mongo tests

--- src/main/java/com/example/spring_reactor_examaple/repository/ItemReactiveRepository.java
+++ src/main/java/com/example/spring_reactor_examaple/repository/ItemReactiveRepository.java
@@ -2,6 +2,8 @@ package com.example.spring_reactor_examaple.repository;
 
 import com.example.spring_reactor_examaple.document.Item;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Mono;
 
 public interface ItemReactiveRepository extends ReactiveMongoRepository<Item, String> {
+    Mono<Item> findByDescription(String description);
 }
